6aeb837fb6610efd4820da60f923769ba39c461d
Денис <puteec228@gmail.com>
2024-02-29 18:07:02 +0300

    Сделяль V2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+
     private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "1675";
@@ -15,11 +15,11 @@ public class Util {
     public Connection getCOnnection() {
         Connection connection = null;
         try {
-            Class.forName(DB_DRIVER);
+
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
             System.out.println("Connection was successful");
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch ( SQLException e) {
             e.printStackTrace();
             System.out.println("Connection ERROR");
         }
