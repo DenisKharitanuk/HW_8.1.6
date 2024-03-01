908a847759dfde8e1c2d9fd087bdc31f177b1317
Денис <puteec228@gmail.com>
2024-03-01 20:48:31 +0300

    добавил  закрытие и открытие коннекшенав блок try

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,9 +14,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Connection connection = new Util().getCOnnection();
 
-        try (Statement statement = connection.createStatement()) {
+
+        try (Connection connection = new Util().getCOnnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS Users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(20), age TINYINT)");
             System.out.println("Таблица User создана");
         } catch (SQLException e) {
@@ -26,8 +26,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        Connection connection = new Util().getCOnnection();
-        try (Statement statement = connection.createStatement()) {
+
+        try (Connection connection = new Util().getCOnnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate("DROP TABLE IF EXISTS Users");
             System.out.println("Таблица удалена");
         } catch (SQLException e) {
@@ -38,8 +38,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Connection connection = new Util().getCOnnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Users (name, lastName, age) values (?,?,?)")) {
+
+        try (Connection connection = new Util().getCOnnection(); PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Users (name, lastName, age) values (?,?,?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -53,8 +53,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        Connection connection = new Util().getCOnnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Users WHERE id = ?")) {
+
+        try (Connection connection = new Util().getCOnnection(); PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Users WHERE id = ?")) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
             System.out.println("Строка удалена ");
@@ -66,10 +66,10 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        Connection connection = new Util().getCOnnection();
+
 
         List<User> users = new ArrayList<>();
-        try (ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM Users")) {
+        try (Connection connection = new Util().getCOnnection(); ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM Users")) {
             while (resultSet.next()) {
                 User user = new User(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4));
                 users.add(user);
@@ -84,8 +84,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Connection connection = new Util().getCOnnection();
-        try (Statement statement = connection.createStatement()) {
+
+        try (Connection connection = new Util().getCOnnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE Users");
             System.out.println("Таблица очищена");
         } catch (SQLException e) {
